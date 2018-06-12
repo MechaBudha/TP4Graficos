260e0baf2f5e2c9ef539b939df9cde52c63f143c
Unknown <tmugetti@gmail.com>
2018-06-11 23:00:42 -0300

    Header inicial creado

--- MugettiSimpleScoreLib/MugettiSimpleScoreLib/MugettiSimpleScoreLib.h
+++ MugettiSimpleScoreLib/MugettiSimpleScoreLib/MugettiSimpleScoreLib.h
@@ -1,8 +1,25 @@
-#pragma once
+#ifndef SCORELIB_H
+#define SCORELIB_H
+#define NOMTAM 3
+#define BOARDTAM 10
+
+
+struct Score
+{
+	int sco;
+	char nom[NOMTAM];
+};
 class MugettiSimpleScoreLib
 {
+	Score _MaxScore;
+	Score _CurScore;
 public:
 	MugettiSimpleScoreLib();
+	void Init();
+	bool RecordScore(int score, char name[NOMTAM]);
+	void GetMaxScore(int& score, char* name[NOMTAM]);
+	void GetCurrentScore(int& score, char* name[NOMTAM]);
 	~MugettiSimpleScoreLib();
 };
+#endif // !SCORELIB_H
 
